ef49ccb17b307158616dee3d7c1bff983fd0d2b1
PitPietro <pit.pietro@protonmail.com>
2020-04-20 16:54:03 +0200

    1 file modified

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-product-details',
   templateUrl: './product-details.component.html',
