b10268b94b58b83000ffdb9f868756daa34112d3
PitPietro <pit.pietro@protonmail.com>
2020-04-20 16:09:21 +0200

    1 file modified

--- src/app/product-alerts/product-alerts.component.ts
+++ src/app/product-alerts/product-alerts.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Input } from '@angular/core';
+import { Output, EventEmitter } from '@angular/core';
 
 /* the decorator indicates that the following class is a component. It provides metadata about the component, including its selector, templates, and styles.
 */
