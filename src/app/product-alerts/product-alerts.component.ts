1de6ed81f16859491d6c7d8093b58043ce73e0f4
PitPietro <pit.pietro@protonmail.com>
2020-04-20 16:05:35 +0200

    1 file modified, 1 added

--- src/app/product-alerts/product-alerts.component.ts
+++ src/app/product-alerts/product-alerts.component.ts
@@ -1,11 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { Input } from '@angular/core';
 
+/* the decorator indicates that the following class is a component. It provides metadata about the component, including its selector, templates, and styles.
+*/
 @Component({
   selector: 'app-product-alerts',
   templateUrl: './product-alerts.component.html',
   styleUrls: ['./product-alerts.component.css']
 })
 export class ProductAlertsComponent implements OnInit {
+
+  @Input() product;
 
   constructor() { }
 
