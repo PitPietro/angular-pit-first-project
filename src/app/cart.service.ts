ebc1ebcfb370c932c2aaba48daa7df8d0bf8a84c
PitPietro <pit.pietro@protonmail.com>
2020-04-20 17:33:45 +0200

    1 file modified

---
+++ src/app/cart.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class CartService {
+
+  constructor() { }
+
+  /* store the array of the current products in the cart. */
+  items = [];
+
+  /* appends a product to an array of items */
+  addToCat(product) {
+    this.items.push(product);
+  }
+
+  /*  collects the items users add to the cart and returns each item with its associated quantity. */
+  getItems() {
+    return this.items;
+  }
+
+  /* */
+  clearCart() {
+    this.items = []
+    return this.items;
+  }
+
+}
